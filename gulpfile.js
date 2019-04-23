91f9c754538d04b807228bd9da986659a7f68566
Christian Pucci <minimamente.info@gmail.com>
2019-04-23 11:43:14 +0200

    Improved sourcemaps and gulp file :sparkles:

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 /**
  * Gulp
  *
- * npm install --save-dev gulp gulp-sass gulp-postcss autoprefixer cssnano gulp-sourcemaps gulp-rename
+ * npm install --save-dev gulp gulp-sass gulp-postcss autoprefixer gulp-rename
  *
  * @see https://gulpjs.com/
  */
@@ -11,7 +11,6 @@ const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 
 // Define project paths
@@ -26,22 +25,23 @@ let paths = {
 
 // SCSS
 function style() {
-    return src(paths.styles.src)
-        // Init sourcemaps before compile files
-        .pipe(sourcemaps.init())
+    return src(paths.styles.src, { sourcemaps: true })
         .pipe(sass().on("error", sass.logError))
-        // Use postcss with autoprefixer
         .pipe(postcss([ autoprefixer() ]))
-        .pipe(dest(paths.styles.dest))
-        .pipe(postcss([ cssnano() ]))
-        // Now add/write the sourcemaps
-        .pipe(sourcemaps.write())
-        .pipe(rename({ extname: '.min.css' }))
-        .pipe(dest(paths.styles.dest));
+        .pipe(dest(paths.styles.dest, { sourcemaps: '.' }))
 }
+
+function styleMin() {
+    return src(paths.styles.src, { sourcemaps: true })
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postcss([ autoprefixer(), cssnano() ]))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(dest(paths.styles.dest, { sourcemaps: '.' }));
+}
+
 
 // Watchers
 watch('assets/scss/*.scss', style);
 
 // Yo baby!! ✌️
-exports.default = parallel( style );
+exports.default = parallel( style, styleMin );
